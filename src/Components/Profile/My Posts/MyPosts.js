cafec2c5a1a059581b60a5ad521c2efadc4005a0
Yakiv Sapa <sapa.yakiv@gmail.com>
2023-12-23 22:50:58 +0100

    modified:   src/App.js 	modified:   src/Components/Profile/My Posts/MyPosts.js 	modified:   src/Components/Profile/Profile.js 	modified:   src/redux/state.js 	modified:   src/render.js

--- src/Components/Profile/My Posts/MyPosts.js
+++ src/Components/Profile/My Posts/MyPosts.js
@@ -3,26 +3,32 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-
-
     let PostsElements = props.posts.map(p => <Post message={p.message} likeCount={p.likesCount} />
     );
+
     let newPostElement = React.createRef();
+
     let addPost = () => {
+        props.addPost();
+    };
+
+    let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.addPost(text);
+        props.updateNewPostText(text);
+        // console.log(text);
     };
 
     return (
         <div className={s.postsBlock}>
-            My posts
+            <h3>My posts</h3>
             <div>
-                New Post
+                <div>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
+                </div>
+                <div>
+                    <button onClick={addPost}>Add Post</button>
+                </div>
             </div>
-            <div>
-                <textarea ref={newPostElement}></textarea>
-            </div>
-            <button onClick={addPost}>Add Post</button>
             <div className={s.posts}>
                 {PostsElements}
             </div>
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,12 +1,17 @@
 import MyPosts from './My Posts/MyPosts';
-import s from './Profile.module.css';
+// import s from './Profile.module.css';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = (props) => {
     return (
         <div>
             <ProfileInfo />
-            <MyPosts posts={props.state.posts} addPost={props.addPost} />
+            <MyPosts
+                posts={props.profilePage.posts}
+                newPostText={props.profilePage.newPostText}
+                updateNewPostText={props.updateNewPostText}
+                addPost={props.addPost}
+            />
         </div>
     );
 };
