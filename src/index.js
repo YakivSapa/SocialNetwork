9d0aee0506a5aaa1fa169cc840b65071d5b8cc36
Yakiv Sapa <sapa.yakiv@gmail.com>
2023-12-25 13:55:29 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js 	modified:   src/redux/state.js 	deleted:    src/render.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render.js';
-import state from './redux/state';
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { addPost, updateNewPostText } from './redux/state';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+let rerenderEntireTree = (state) => {
+    root.render(   // root / ReactDOM
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+        </React.StrictMode>
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
