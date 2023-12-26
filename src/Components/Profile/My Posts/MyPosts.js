1c1770196853f420c8d143045653cb477c7f4b15
Yakiv Sapa <sapa.yakiv@gmail.com>
2023-12-26 21:26:38 +0100

    modified:   src/Components/Profile/My Posts/MyPosts.js 	modified:   src/redux/state.js

--- src/Components/Profile/My Posts/MyPosts.js
+++ src/Components/Profile/My Posts/MyPosts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
 
 const MyPosts = (props) => {
     let PostsElements = props.posts.map(p => <Post message={p.message} likeCount={p.likesCount} />
@@ -9,12 +10,13 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({ type: 'ADD-POST' });
+        props.dispatch(addPostActionCreator());
     };
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        // let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
     };
 
