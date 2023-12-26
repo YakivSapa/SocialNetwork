6a622e6ed91f7d0011445610b196a2e77764d50c
Yakiv Sapa <sapa.yakiv@gmail.com>
2023-12-26 19:38:59 +0100

    modified:   src/App.js 	modified:   src/Components/Profile/My Posts/MyPosts.js 	modified:   src/Components/Profile/Profile.js 	modified:   src/index.js 	modified:   src/redux/state.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = createRoot(domNode);
 let rerenderEntireTree = (state) => {
     root.render(   // root / ReactDOM
         <React.StrictMode>
-            <App state={store.getState()} store={store} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
+            <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
         </React.StrictMode>
     );
 }
