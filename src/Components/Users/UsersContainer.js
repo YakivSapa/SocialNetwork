1ca3c94ae3beb98de629800bbee662a6b7111cd2
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-01-18 17:17:59 +0100

    modified:   src/Components/Users/Users.js 	modified:   src/Components/Users/Users.module.css 	modified:   src/Components/Users/UsersContainer.js 	modified:   src/redux/usersReducer.js

--- src/Components/Users/UsersContainer.js
+++ src/Components/Users/UsersContainer.js
@@ -1,10 +1,13 @@
 import { connect } from "react-redux";
 import Users from './Users';
-import { followAC, setUsersAC, unFollowAC } from "../../redux/usersReducer";
+import { followAC, setCurrentPageAC, setUsersAC, unFollowAC, setTotalUsersCountAC } from "../../redux/usersReducer";
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -17,6 +20,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 }
