8efa07ec9878dfe1a320234a843c0313a13c7ca5
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-01-19 22:04:46 +0100

    modified:   src/Components/Users/UsersContainer.js 	new file:   src/Components/common/Preloader/preloader.js 	modified:   src/redux/usersReducer.js

---
+++ src/Components/common/Preloader/preloader.js
@@ -0,0 +1,7 @@
+let Preloader = (props) => {
+    return <div>
+        <img style={{ width: 100, height: 100 }} src="https://www.wpfaster.org/wp-content/uploads/2013/06/loading-gif.gif" />
+    </div>
+}
+
+export default Preloader;
