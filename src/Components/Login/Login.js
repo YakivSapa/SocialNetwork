5389bdbe4f92daa4e3767d04e704cba204043528
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-03-13 16:37:23 +0100

    modified:   src/Components/Login/Login.js 	modified:   src/api/api.js 	modified:   src/redux/authReducer.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,18 @@
 import { Field, reduxForm } from "redux-form"
 import { Input } from "../common/FormsControls/FormsControls"
 import { required } from "../utils/validators/validators"
+import { connect } from "react-redux"
+import { login } from "../../redux/authReducer"
+import { Navigate } from "react-router-dom"
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name={"login"} validate={[required]} component={Input} />
+                <Field placeholder={"Email"} name={"email"} validate={[required]} component={Input} />
             </div>
             <div>
-                <Field placeholder={"Password"} name={"password"} validate={[required]} component={Input} />
+                <Field placeholder={"Password"} name={"password"} type={"password"} validate={[required]} component={Input} />
             </div>
             <div>
                 <Field component={Input} name={"rememberMe"} type={"checkbox"} /> Remember me
@@ -24,13 +27,18 @@ const LoginForm = (props) => {
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
-    const onSubmit = (FormData) => {
-        console.log(FormData);
+    const onSubmit = (formData) => {
+        props.login(formData.email, formData.password, formData.rememberMe);
+    }
+    if (props.isAuth) {
+        return <Navigate to={"/profile"} />
     }
     return <div>
         <h1>Login</h1>
         <LoginReduxForm onSubmit={onSubmit} />
     </div>
 }
-
-export default Login;
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+export default connect(mapStateToProps, { login })(Login);
