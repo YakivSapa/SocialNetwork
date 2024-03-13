90d3ee13c4880520a1c65e900b90dfde78708e9a
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-03-13 16:56:14 +0100

    modified:   src/Components/Header/Header.js 	modified:   src/Components/Header/HeaderContainer.js 	modified:   src/redux/authReducer.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,8 +6,9 @@ const Header = (props) => {
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.7acp2NExpYoVMo6rbOUYNQHaHS%26pid%3DApi&f=1&ipt=72abbfa144715c21b44e876075c324fcf8b90a968959366a029c94396111bacd&ipo=images"></img>
 
         <div className={s.loginBlock}>
-            {props.isAuth ? props.login :
-                <NavLink to={'/login'}>Login</NavLink>}
+            {props.isAuth
+                ? <div>{props.login} - <button onClick={props.logout}>Log out</button> </div>
+                : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
     );
