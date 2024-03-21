a230d958dd3e2403fc1b086ebc377e311328c9cc
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-03-21 20:53:26 +0100

    modified:   src/redux/profileReducer.js 	new file:   src/redux/profileReducer.test.js

---
+++ src/redux/profileReducer.test.js
@@ -0,0 +1,37 @@
+import profileReducer, { addPostActionCreator } from "./profileReducer";
+
+let state = {
+    posts: [
+        { id: 1, message: 'Hi. How are you?', likesCount: 20 },
+        { id: 2, message: 'It\'s my first post.', likesCount: 15 }
+    ]
+};
+
+it('length of posts should be incremented', () => {
+    // 1. Testing data
+    let action = addPostActionCreator("TestingProfileReducer");
+    // 2. Action
+    let newState = profileReducer(state, action);
+    // 3. Expectations
+    expect(newState.posts.length).toBe(5);
+});
+
+it('message of new post should be correct', () => {
+    // 1. Testing data
+    let action = addPostActionCreator("TestingProfileReducer");
+    // 2. Action
+    let newState = profileReducer(state, action);
+    // 3. Expectations
+    expect(newState.posts[4].message).toBe("TestingProfileReducer");
+});
+
+it('after deleting, length of messages should be decremented', () => {
+    // 1. Test data
+    let action = deletePost(1);
+
+    // 2. Action
+    let newState = profileReducer(state, action)
+
+    // 3. Expectations
+    expect(newState.posts.length).toBe(3);
+})
