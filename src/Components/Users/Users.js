af687c33a8672318eba813764dd42f88338a9d18
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-03-29 18:49:45 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Components/Users/Users.js 	modified:   src/Components/common/Paginator/Paginator.jsx 	modified:   src/Components/common/Paginator/Paginator.module.css 	modified:   src/redux/usersReducer.js

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -4,13 +4,16 @@ import User from './User';
 
 let Users = ({ currentPage, totalUsersCount, pageSize, onPageChanged, users, ...props }) => {
     return <div>
-        <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalUsersCount={totalUsersCount} pageSize={pageSize} />
-        {
-            users.map(u => <User
-                user={u} followingInProgress={props.followingInProgress} key={u.id} unfollow={props.unfollow} follow={props.follow}
-            />
-            )
-        }
+        <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
+            totalItemsCount={totalUsersCount} pageSize={pageSize} />
+        <div>
+            {
+                users.map(u => <User
+                    user={u} followingInProgress={props.followingInProgress} key={u.id} unfollow={props.unfollow} follow={props.follow}
+                />
+                )
+            }
+        </div>
     </div>
 }
 
