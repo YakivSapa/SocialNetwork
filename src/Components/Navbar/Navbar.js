849967c2de8c5ca514cb1dac50dc98545588ce8e
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-04-18 18:21:37 +0200

    modified:   src/Components/Navbar/Navbar.js 	modified:   src/Components/Navbar/Navbar.module.css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,22 +4,22 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div>
+            <div className={s.navItems}>
                 <NavLink to="/profile" className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
             </div>
-            <div>
+            <div className={s.navItems}>
                 <NavLink to="/dialogs" className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink>
             </div>
-            <div>
+            <div className={s.navItems}>
                 <NavLink to="/users" className={navData => navData.isActive ? s.active : s.item}>Users</NavLink>
             </div>
-            <div>
+            <div className={s.navItems}>
                 <NavLink to='/news' className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
             </div>
-            <div>
+            <div className={s.navItems}>
                 <NavLink to='/music' className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
             </div>
-            <div>
+            <div className={s.navItems}>
                 <NavLink to="/settings" className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
             </div>
         </nav>
