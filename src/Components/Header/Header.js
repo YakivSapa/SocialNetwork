07943ad7e30505cceb616a76ffb686905bb84ba8
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-04-18 17:51:03 +0200

    modified:   src/Components/Header/Header.js 	modified:   src/Components/Header/HeaderContainer.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
-import userPhoto from "../../assets/images/default-profile-pic.jpg";
 
 const Header = (props) => {
     return (<header className={s.header}>
         <div className={s.loginBlock}>
             {props.isAuth
-                ? <div><img src={props.userPhoto || userPhoto} /> - <button onClick={props.logout}>Log out</button> </div>
+                ? <div> Welcome, {props.login} - <button onClick={props.logout}>Log out</button> </div>
                 : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
--- src/Components/Header/HeaderContainer.js
+++ src/Components/Header/HeaderContainer.js
@@ -4,16 +4,14 @@ import { connect } from 'react-redux';
 import { logout } from '../../redux/authReducer';
 
 class HeaderContainer extends React.Component {
-
     render() {
-        return <Header {...this.props} userPhoto={this.props.userPhoto} />
+        return <Header {...this.props} />
     }
 };
 
 let mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
-    login: state.auth.login,
-    userPhoto: state.profilePage.profile
+    login: state.auth.login
 });
 
 export default connect(mapStateToProps, { logout })(HeaderContainer);
