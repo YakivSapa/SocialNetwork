9959a1c5b5f0730f6c5cfd7189b703d287d94982
Yakiv Sapa <sapa.yakiv@gmail.com>
2024-04-20 18:05:26 +0200

    modified:   src/Components/Header/Header.js 	modified:   src/Components/Header/Header.module.css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,8 +5,8 @@ const Header = (props) => {
     return (<header className={s.header}>
         <div className={s.loginBlock}>
             {props.isAuth
-                ? <div> Welcome, {props.login} - <button onClick={props.logout}>Log out</button> </div>
-                : <NavLink to={'/login'}>Login</NavLink>}
+                ? <div> Welcome, {props.login} - <button onClick={props.logout} className={s.button}>Log out</button> </div>
+                : <NavLink to={'/login'} className={s.button}>Login</NavLink>}
         </div>
     </header>
     );
